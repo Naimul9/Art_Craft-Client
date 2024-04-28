a4558737bceb8ca7d519eb8171a151523bd21c4e
Naimul9 <naimulakib100@gmail.com>
2024-04-28 20:18:09 +0600

    dark theme added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,12 @@ const Home = () => {
     return (
         <div>
             <h1 className="font-semibold text-6xl mt-3 bg-stone-200 text-center py-3">THE GALLERY</h1>
-         <Slider></Slider>
+            <Slider></Slider>
+
+            <div className="mt-10">
+               <h1 className="font-semibold text-4xl text-white text-center bg-black py-5 "> CRAFT ITEMS</h1>
+            </div>
+
         </div>
     );
 };
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,38 +2,35 @@
 
 const Slider = () => {
     return (
-        <div className="max-w-screen-2xl mx-auto mt-10 ">
+      <section className="q
+       max-w-screen-2xl mt-16 mb-4 ">
+          <div className="max-w-screen-xl mx-auto ">
          <div className="carousel rounded-2xl   ">
         <div id="slide1" className="carousel-item relative w-full">
-          <img src="https://i.ibb.co/s5JsXC2/pexels-expect-best-323780.jpg" className="w-full h-[600px]" />
+          <img src="https://i.ibb.co/0V0zg57/pexels-shahram-jahansooz-180917-5113153.jpg" className="w-full h-[600px]" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className="btn btn-circle">❮</a> 
             <a href="#slide2" className="btn btn-circle">❯</a>
           </div>
         </div> 
         <div id="slide2" className="carousel-item relative w-full">
-          <img src="https://i.ibb.co/K2tKRqd/pexels-binyamin-mellish-1396122.jpg" className="w-full h-[600px]" />
+          <img src="https://i.ibb.co/TY4yyRt/pexels-dreamypixel-580151.jpg" className="w-full h-[600px]" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide1" className="btn btn-circle">❮</a> 
             <a href="#slide3" className="btn btn-circle">❯</a>
           </div>
         </div> 
         <div id="slide3" className="carousel-item relative w-full">
-          <img src="https://i.ibb.co/tLYGKYC/pexels-kyla-rose-rockola-816198.jpg" className="w-full h-[600px]" />
+          <img src="https://i.ibb.co/fYRbZhG/pexels-navneet-shanu-202773-672630.jpg" className="w-full h-[600px]" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" className="btn btn-circle">❮</a> 
             <a href="#slide4" className="btn btn-circle">❯</a>
           </div>
         </div> 
-        <div id="slide4" className="carousel-item relative w-full"> 
-          <img src="https://i.ibb.co/LZqFC5P/pexels-pixabay-276554.jpg" className="w-full h-[600px]" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" className="btn btn-circle">❮</a> 
-            <a href="#slide1" className="btn btn-circle">❯</a>
-          </div>
-        </div>
+       
       </div>
        </div>
+      </section>
     );
 };
 
