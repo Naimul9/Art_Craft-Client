bb56f92f5910b0c326d377cdda862fbd6be2289d
Naimul9 <naimulakib100@gmail.com>
2024-04-29 03:30:24 +0600

    getting data from database

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,10 @@
+import { useLoaderData } from "react-router-dom";
 import Slider from "./Slider";
 
 const Home = () => {
+
+    const craft = useLoaderData()
+
     return (
         <div>
             <h1 className="font-semibold text-6xl mt-3 bg-stone-200 text-center py-3">THE GALLERY</h1>
@@ -8,6 +12,8 @@ const Home = () => {
 
             <div className="mt-10">
                <h1 className="font-semibold text-4xl text-white text-center bg-black py-5 "> CRAFT ITEMS</h1>
+
+<p> {craft.length} </p>
             </div>
 
         </div>
